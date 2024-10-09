3339c358acc86a1a7d4d501080d2a63ab78d8300
sachrulakbar <145645888+sachrulakbar@users.noreply.github.com>
2024-10-09 11:59:28 +0700

    Latihan 1 Praktikum Basis Data

---
+++ latihan 1 sachrul akbar.sql
@@ -0,0 +1,7 @@
+INSERT INTO MAHASISWA (ID, NIM, NAMA, JURUSAN, TANGGAL_LAHIR)
+VALUES
+	(2, 233040001, 'Kresna Satria', 'Teknik Informatika', '2004-10-01'),
+	(3, 233040018, 'Haikal Al Ghazy', 'Teknik Informatika', '2004-12-02'),
+	(4, 233040035, 'Moreno Wisesa', 'Teknik Informatika', '2004-07-04'),
+	(5, 233040027, 'Tyezar Hasan', 'Teknik Informatika', '2004-08-05'),
+	(6, 233040017, 'Aqillah Lean', 'Teknik Informatika', '2004-11-17');
